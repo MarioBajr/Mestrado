f24429a29f0304b7773e90c5c7cee9e130ae2e9d
Mario Barbosa <Mario@Marios-MacBook.local>
2013-12-31 22:10:47 -0300

    finishing first graph extractor version

--- MapRoute/geometry.py
+++ MapRoute/geometry.py
@@ -1,21 +1,14 @@
 __author__ = 'Mario'
 
-
-import sympy as sy
 import numpy as np
 import math
+
 
 class Rectangle(object):
 
     def __init__(self, p1, p2):
-        p1 = Point(p1.x, p1.y)
-        p2 = Point(p2.x, p2.y)
-        if (p1.x > p2.x) is True:
-            self.p1, self.p2 = p2, p1
-        elif (p1.x == p2.x) is (p1.y > p2.y) is True:
-            self.p1, self.p2 = p2, p1
-        else:
-            self.p1, self.p2 = p1, p2
+        self.p1 = Point(min(p1.x, p2.x), min(p1.y, p2.y))
+        self.p2 = Point(max(p1.x, p2.x), max(p1.y, p2.y))
 
     def is_inside(self, r):
         return self.p1.x <= r.p1.x and self.p2.x >= r.p2.x and self.p1.y <= r.p1.y and self.p2.y >= r.p2.y
@@ -34,47 +27,42 @@ class Rectangle(object):
         return (ax1 or ax2 or bx1 or bx2) and (ay1 or ay2 or by1 or by2)
 
     def inflate(self, d):
+        d = int(d)
         self.p1.x -= d
-        self.p2.y -= d
-
-        #sy.Point(self.p1.x-d, self.p1.y-d)
+        self.p1.y -= d
         self.p2.x += d
         self.p2.y += d
-
-        #sy.Point(self.p2.x+d, self.p2.y+d)
 
     @staticmethod
     def from_segment(s):
         return Rectangle(s.p1, s.p2)
 
+    def __str__(self):
+        return type(self).__name__ + "(" + self.p1.__str__() + ", " + self.p2.__str__() + ")"
+
 
 class Point(object):
-
-    # def __init__(cls, *args, **kws):
-    #     cls.segments = []
-    #     return sy.Point.__init__(cls, *args, **kws)
-
-    # def __new__(cls, *args, **kwargs):
-    #     return sy.Point.__new__(cls, *args, **kwargs)
-
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.segments = []
-
-    def add_segment(self, segment):
-        self.segments.append(segment)
-
-    # def remove_segment(self, segment):
-    #     self.segments.remove(segment)
+        self.segments = set([])
 
     def is_joint(self):
         return len(self.segments) > 1
 
+    def to_tuple(self):
+        return int(self.x), int(self.y)
+
+    @staticmethod
+    def interpolate(p1, p2, f):
+        x = p1.x + (p2.x - p1.x)*f
+        y = p1.y + (p2.y - p1.y)*f
+        return Point(x, y)
+
     @staticmethod
     def min(*args, **kwargs):
         def min_tuple(a, b):
-            if a.x < b.y:
+            if a.x < b.x:
                 return a
             elif a.x == b.x and a.y < b.y:
                 return a
@@ -107,10 +95,27 @@ class Point(object):
         return x1*x2 + y1*y2
 
     @staticmethod
+    def squared_distance(p1, p2):
+        x = p1.x - p2.x
+        y = p1.y - p2.y
+        return x*x + y*y
+
+    @staticmethod
     def distance(p1, p2):
         x = p1.x - p2.x
         y = p1.y - p2.y
         return math.sqrt(x*x + y*y)
+
+    @staticmethod
+    def is_collinear(p1, p2, p3):
+        v1 = p2 - p1
+        v2 = p3 - p1
+        a = math.acos(v1.dot(v2)/(abs(v1)*abs(v2)))
+        return a == 0
+
+    @staticmethod
+    def zero():
+        return Point(0, 0)
 
     def __add__(self, other):
         return Point(self.x+other.x, self.y+other.y)
@@ -139,28 +144,27 @@ class Point(object):
 
 class Segment(object):
 
-    # def __init__(cls, *args, **kws):
-    #     cls.pt1 = args[0]
-    #     cls.pt2 = args[1]
-    #     return sy.Point.__init__(cls, *args, **kws)
-    #
-    # def __new__(cls, p1, p2, *args, **kwargs):
-    #     s = sy.Segment.__new__(cls, p1, p2, **kwargs)
-    #     cls.pt1 = p1
-    #     cls.pt2 = p2
-    #     p1.add_segment(s)
-    #     p2.add_segment(s)
-    #     return s
-
     def __init__(self, p1, p2):
         self._p1 = p1
         self._p2 = p2
-        p1.segments.append(self)
-        p2.segments.append(self)
+        self._update()
 
-    def __del__(self):
+    def unlink(self):
         self._p1.segments.remove(self)
         self._p2.segments.remove(self)
+
+    @property
+    def squared_length(self):
+        return Point.squared_distance(self._p1, self._p2)
+
+
+    @property
+    def length(self):
+        return Point.distance(self._p1, self._p2)
+
+    @property
+    def center(self):
+        return (self.p2+self.p1)/2
 
     @property
     def p1(self):
@@ -169,8 +173,9 @@ class Segment(object):
     @p1.setter
     def p1(self, value):
         self._p1.segments.remove(self)
-        value.segments.append(self)
+        value.segments.add(self)
         self._p1 = value
+        self._update()
 
     @property
     def p2(self):
@@ -178,14 +183,27 @@ class Segment(object):
 
     @p2.setter
     def p2(self, value):
+        self._p2.segments.remove(self)
+        value.segments.add(self)
         self._p2 = value
+        self._update()
+
+    def _update(self):
+        p1, p2 = self._p1, self._p2
+        self._p1 = Point.min(p1, p2)
+        self._p2 = Point.max(p1, p2)
+        self._p1.segments.add(self)
+        self._p2.segments.add(self)
 
     def is_connected_with(self, s):
-        c1 = self.connections
+        c1 = s.connections
         c2 = self.connections
         return s in self.connections or len(c1.intersection(c2)) > 1
 
     def distance(self, p):
+        return math.sqrt(self.squared_distance(p))
+
+    def squared_distance(self, p):
         x1, y1 = self.p1.x, self.p1.y
         x2, y2 = self.p2.x, self.p2.y
         x3, y3 = p.x, p.y
@@ -208,25 +226,55 @@ class Segment(object):
         dx = x - x3
         dy = y - y3
 
-        dist = math.sqrt(dx*dx + dy*dy)
-        return dist
+        return dx*dx + dy*dy
 
     def intersection(self, s):
-        def perp(a):
-            return Point(-a.y, a.x)
 
-        a1 = self.p1
-        a2 = self.p2
-        b1 = self.p1
-        b2 = self.p2
-        da = a2-a1
-        db = b2-b1
-        dp = a1-b1
-        dap = perp(da)
-        denom = dap.dot(db)#np.dot( dap, db)
-        num = dap.dot(dp)#np.dot( dap, dp )
-        r = (num / denom)*db + b1
-        return r
+        def inter_func(s1, s2):
+            a = s1.p1
+            b = s1.p2
+            c = s2.p1
+            d = s2.p2
+            f_ab = (d.x - c.x) * (a.y - c.y) - (d.y - c.y) * (a.x - c.x)
+
+            if f_ab == 0:
+                return float('inf')
+
+            f_cd = (b.x - a.x) * (a.y - c.y) - (b.y - a.y) * (a.x - c.x)
+            f_d = (d.y - c.y) * (b.x - a.x) - (d.x - c.x) * (b.y - a.y)
+
+            if f_d == 0:
+                return float('inf')
+
+            f1 = f_ab/float(f_d)
+            f2 = f_cd/float(f_d)
+
+            if (f1 <= 0 or f1 >= 1):
+                return float('inf')
+            if (f2 <= 0 or f2 >= 1):
+                return float('inf')
+            return f1;
+
+        f = inter_func(self, s)
+
+        if f == float('inf') or f <= 0 or f >= 1:
+            return None
+
+        return self.interpolate(f)
+
+    def perpendicular_segment(self, p):
+        d = self.p1 - self.p2
+        d1, d2 = d.x, d.y
+        if d2 == 0:  # If a horizontal line
+            if p.y == self.p1.y:  # if p is on this linear entity
+                return Segment(p, p + Point(0, 1))
+            else:
+                p2 = Point(p.x, self.p1.y)
+                return Segment(p, p2)
+        else:
+            p2 = Point(p.x - d2, p.y + 1)
+            s = Segment(p, p2)
+            return Segment(p, p2)
 
     @property
     def bounds(self):
@@ -234,92 +282,31 @@ class Segment(object):
 
     @property
     def connections(self):
-        all_segments = set(self.p1.segments+self.p2.segments)
+        all_segments = self.p1.segments.union(self.p2.segments)
         return all_segments
 
-    # @property
-    # def pt1(self):
-    #     self.pt1
-
-
-    # @property
-    # def pt2(self):
-    #     self.pt2
+    def interpolate(self, f):
+        return Point.interpolate(self.p1, self.p2, f)
 
     @staticmethod
     def union(s1, s2):
-        p1 = Point.min(s1.p1, s1.p2, s2.p1, s2.p2)
-        p2 = Point.max(s1.p1, s1.p2, s2.p1, s2.p2)
-        print "UNION MIN", s1.p1, s1.p2, s2.p1, s2.p2, ">", p1
-        print "UNION MAX", s1.p1, s1.p2, s2.p1, s2.p2, ">", p2
+        def compare(a, b):
+            return int(a[2]-b[2])
+
+        l = [(s1.p1, s2.p1), (s1.p1, s2.p2), (s1.p2, s2.p1), (s1.p2, s2.p2), (s1.p1, s1.p2), (s2.p1, s2.p2)]#TODO
+        l = [(p1, p2, Point.distance(p1, p2)) for p1, p2 in l]
+        l = sorted(l, cmp=compare)
+        p1, p2, d = l[-1]
+
         return Segment(p1, p2)
 
     @staticmethod
     def angle_between(s1, s2):
         v1 = s1.p2 - s1.p1
         v2 = s2.p2 - s2.p1
-        return math.acos(v1.dot(v2)/(abs(v1)*abs(v2)))
+        a = v1.dot(v2)/(abs(v1)*abs(v2))
+        a = max(min(a,  1), -1)
+        return math.acos(a)
 
     def __str__(self):
         return type(self).__name__ + "(" + self.p1.__str__() + ", " + self.p2.__str__() + ")"
-
-# class Point(object):
-#
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#         self.connected = False
-#
-#     def to_numpy(self):
-#         return np.array([self.x, self.y])
-#
-#     def from_numpy(self, a):
-#         return Point(a[0], a[1])
-#
-#     @staticmethod
-#     def zero():
-#         return Point(0, 0)
-#
-#     def distance(a, b):
-#         x = a.x-b.x
-#         y = a.y-b.y
-#         return math.sqrt(x*x+y*y)
-#
-# class Segment(object):
-#
-#     def __init__(self, p1, p2):
-#         self.p1 = p1
-#         self.p2 = p2
-#
-#     def cartesian_representation(self):
-#         a = math.atan2(self.p2.y-self.p1.y, self.p2.x-self.p1.x)
-#         b = (self.p2.x*self.p1.y-self.p1.x*self.p2.y)/(self.p2.x-self.p1.x)
-#         return a, b
-#
-#     @staticmethod
-#     def __ccw(a, b, c):
-#         return (c.y-a.y) * (b.x-a.x) > (b.y-a.y) * (c.x-a.x)
-#
-#     def intersect(l1, l2):
-#         a = l1.p1, b = l1.p2, c = l2.p1, d = l2.p2
-#         return Segment.__ccw(a, c, d) != Segment.__ccw(b, c, d) and Segment.__ccw(a, b, c) != Segment.__ccw(a, b, d)
-#
-#     def intersection(a, b):
-#         def perp(a) :
-#             b = np.empty_like(a)
-#             b[0] = -a[1]
-#             b[1] = a[0]
-#             return b
-#
-#         a1 = a.p1.to_numpy()
-#         a2 = a.p2.to_numpy()
-#         b1 = b.p1.to_numpy()
-#         b2 = b.p2.to_numpy()
-#         da = a2-a1
-#         db = b2-b1
-#         dp = a1-b1
-#         dap = perp(da)
-#         denom = np.dot( dap, db)
-#         num = np.dot( dap, dp )
-#         r = (num / denom)*db + b1
-#         return Point.from_numpy(r)
--- MapRoute/main.py
+++ MapRoute/main.py
@@ -4,17 +4,15 @@ __author__ = 'Mario'
 import cv2 as cv
 import numpy as np
 from skimage import morphology
-# import sympy as sy
 import os
-import math
 from geometry import *
 
 IMAGE_PATH = 'Resources/1.bmp'
 SKELETON_PATH = 'Resources/sk.png'
 
-LINK_GAP = 10
-ATTRACT_GAP = 20
-INFLATE_GAP = 1.4*ATTRACT_GAP #sqrt(2)*ATTRACT_GAP
+NEW_SEG_GAP = 15
+LINK_SEG_GAP = int(NEW_SEG_GAP/3)
+INFLATE_GAP = NEW_SEG_GAP
 
 NEAR_MIN_DIST = 5
 NEAR_MIN_ANG = np.pi/60
@@ -44,7 +42,12 @@ def process_skeleton():
 
 def clean_segments(segments):
     visited = []
-    list = segments[:]
+
+    def filter_small_segments(s):
+        return s.squared_length > 10
+
+    list = filter(filter_small_segments, segments)
+    print len(segments), len(list)
 
     have_changes = True
     while have_changes:
@@ -63,23 +66,27 @@ def clean_segments(segments):
                 r2 = seg2.bounds
                 r2.inflate(INFLATE_GAP)
 
-                if (not seg1.is_connected_with(seg2)) and r1.collide(r2):
+                if r1.collide(r2):
                     dist_s1 = seg1.distance(seg2.p1)
                     dist_s2 = seg1.distance(seg2.p2)
 
                     dist_s3 = seg2.distance(seg1.p1)
                     dist_s4 = seg2.distance(seg1.p2)
 
-                    min_dist = min(dist_s1+dist_s2, dist_s3+dist_s4)
+                    min_dist = min(dist_s1, dist_s2, dist_s3, dist_s4)
                     angle = Segment.angle_between(seg1, seg2)
 
                     if angle <= NEAR_MIN_ANG and min_dist <= NEAR_MIN_DIST:
                         have_changes = True
                         list.remove(seg1)
                         list.remove(seg2)
+                        seg1.unlink()
+                        seg2.unlink()
 
                         s = Segment.union(seg1, seg2)
                         list.append(s)
+
+                        # print "remove", seg1, seg2, " ===> ", s
 
                 if have_changes:
                     break
@@ -92,20 +99,29 @@ def clean_segments(segments):
 
 def connect_segments(segments):
 
-    print "processing"
+    print "\n\n\nprocessing"
 
     visited = []
     list = segments[:]
 
+    def sorter_func(a, b):
+        da = Point.squared_distance(a.center, Point.zero())
+        db = Point.squared_distance(b.center, Point.zero())
+        return da - db
+
+    list = sorted(list, cmp=sorter_func)
+
     def add_seg(seg):
         if isinstance(seg, Segment):
             list.append(seg)
-            print "add -----> ", seg
+
+    def rm_seg(seg):
+        seg.unlink()
+        list.remove(seg)
 
     have_changes = True
     while have_changes:
-        print "loop"
-        print len(list)
+        print "loop ----------------------", len(list)
 
         have_changes = False
         list1 = list[:]
@@ -125,21 +141,17 @@ def connect_segments(segments):
                 r2 = seg2.bounds
                 r2.inflate(INFLATE_GAP)
 
-                print " >", r1.p1, r1.p2, " <> ", r2.p1, r2.p2, " #> ", (not seg1.is_connected_with(seg2)), r1.collide(r2)
+                print " HIT", (not seg1.is_connected_with(seg2)), r1.collide(r2)
 
                 if (not seg1.is_connected_with(seg2)) and r1.collide(r2):
 
-                    inters = seg1.intersection(seg2)
+                    p = seg1.intersection(seg2)
 
-                    if len(inters) > 0:
+                    if p is not None:
+                        print "--\n<Cross>"
                         have_changes = True
-                        print 'inter', seg1, seg2, inters
-                        p = inters[0]
-
-                        p = Point(int(p.x), int(p.y))
-                        list.remove(seg1)
-                        list.remove(seg2)
-                        print "remove", seg1, seg2
+                        rm_seg(seg1)
+                        rm_seg(seg2)
 
                         add_seg(Segment(seg1.p1, p))
                         add_seg(Segment(seg1.p2, p))
@@ -149,25 +161,29 @@ def connect_segments(segments):
                     if have_changes:
                         break
 
-                    dist_a = seg1.p1.distance(seg2.p1)
-                    dist_b = seg1.p1.distance(seg2.p2)
-                    dist_c = seg1.p2.distance(seg2.p1)
-                    dist_d = seg1.p2.distance(seg2.p2)
+                    dist_a = Point.distance(seg1.p1, seg2.p1)
+                    dist_b = Point.distance(seg1.p1, seg2.p2)
+                    dist_c = Point.distance(seg1.p2, seg2.p1)
+                    dist_d = Point.distance(seg1.p2, seg2.p2)
 
                     min_dist = min(dist_a, dist_b, dist_c, dist_d)
 
-                    if min_dist < LINK_GAP:
-                        print 'min', int(min_dist), "|", dist_a, dist_b, dist_c, dist_d
+                    if min_dist < NEW_SEG_GAP:
                         have_changes = True
 
-                        create_segment = min_dist > ATTRACT_GAP
+                        create_segment = min_dist > LINK_SEG_GAP
+
+                        if create_segment:
+                            print "--\n<New Seg>", min_dist
+                        else:
+                            print "--\n<Link>", min_dist
 
                         # if not create_segment:
                         #     list.remove(seg2)
                         #     print "remove", seg2
 
                         if dist_a == min_dist:
-                            print 'A', seg1.p1, seg1.p2, seg2.p1, seg2.p2
+                            print 'A', seg1, seg2
                             if create_segment:
                                 add_seg(Segment(seg1.p1, seg2.p1))
                                 # add_seg(Segment(seg1.p1, seg2.p2))
@@ -199,34 +215,41 @@ def connect_segments(segments):
                     if have_changes:
                         break
 
-                    dist_s1 = seg1.distance(seg2.p1)
-                    dist_s2 = seg1.distance(seg2.p2)
+                    gap = LINK_SEG_GAP*LINK_SEG_GAP
+
+                    dist_s1 = seg1.squared_distance(seg2.p1)
+                    dist_s2 = seg1.squared_distance(seg2.p2)
                     min_dist = min(dist_s1, dist_s2)
 
                     s1 = None
+                    print "min", seg1, seg2, " <> ",  int(dist_s1), int(dist_s2), " >> ", min_dist, gap
 
-                    if min_dist < LINK_GAP:
+                    if min_dist < gap:
                         s1 = seg1
                         s2 = seg2
+                        p = [s2.p1, s2.p2][min_dist == dist_s2]
 
-                    dist_s1 = seg2.distance(seg1.p1)
-                    dist_s2 = seg2.distance(seg1.p2)
+                    dist_s1 = seg2.squared_distance(seg1.p1)
+                    dist_s2 = seg2.squared_distance(seg1.p2)
                     min_dist = min(dist_s1, dist_s2)
 
-                    if min_dist < LINK_GAP:
+                    if min_dist < gap:
                         s1 = seg2
                         s2 = seg1
+                        p = [s2.p1, s2.p2][min_dist == dist_s2]
 
-                    print "min", seg1, seg2, " <> ",  int(dist_s1), int(dist_s2)
+                    print "min", seg1, seg2, " <> ",  int(dist_s1), int(dist_s2), " >> ", min_dist, gap
                     if s1 is not None:
+                        print "--\n<Next>"
                         have_changes = True
 
-                        p = [s2.p1, s2.p2][min_dist == dist_s2]
                         s = s1.perpendicular_segment(p)
-                        p0 = [s.p1, s.p2][sy.Point.is_collinear(s1.p1, s1.p2, s.p1)]
-                        p0 = Point(p0.x, p0.y)
+                        p0 = [s.p1, s.p2][Point.is_collinear(s1.p1, s1.p2, s.p1)]
+                        # p0 = Point(p0.x, p0.y)
 
-                        list.remove(s1)
+                        print s1, s2, ">>", p, s, p0
+
+                        rm_seg(s1)
                         add_seg(Segment(s1.p1, p0))
                         add_seg(Segment(s1.p2, p0))
                         add_seg(Segment(p, p0))
@@ -241,30 +264,48 @@ def connect_segments(segments):
 
     return visited
 
+
 def draw_segments(im, segments):
+
+    for segment in segments:
+        rec = Rectangle.from_segment(segment)
+        rec.inflate(INFLATE_GAP)
+        cv.rectangle(im, rec.p1.to_tuple(), rec.p2.to_tuple(), (100, 0, 0, 0), 1)
+
     for segment in segments:
         x1 = int(segment.p1.x)
         y1 = int(segment.p1.y)
         x2 = int(segment.p2.x)
         y2 = int(segment.p2.y)
 
-        r = np.random.random_sample()*255
-        g = np.random.random_sample()*255
-        b = np.random.random_sample()*255
+        r = int(np.random.random_sample()*255)
+        g = int(np.random.random_sample()*255)
+        b = int(np.random.random_sample()*255)
 
-        color1 = {0:(0, 255, 0), 1:(0, 0, 255)}[segment.p1.is_joint()]
-        color2 = {0:(0, 255, 0), 1:(0, 0, 255)}[segment.p2.is_joint()]
+        color1 = [(0, 255, 0, 255), (0, 0, 255, 255)][segment.p1.is_joint()]
+        color2 = [(0, 255, 0, 255), (0, 0, 255, 255)][segment.p2.is_joint()]
 
-        cv.circle(im, (x1, y1), 5, color1, 1)
-        cv.circle(im, (x2, y2), 5, color2, 1)
-        cv.line(im, (x1, y1), (x2, y2), (r, g, b), 3)
+        cv.circle(im, (x1, y1), NEW_SEG_GAP, color1, 1)
+        cv.circle(im, (x2, y2), NEW_SEG_GAP, color2, 1)
+        cv.line(im, (x1, y1), (x2, y2), (r, g, b, 255), 3)
+
+
+def write_segments(im, filename, segments):
+    im_out = np.copy(im)
+    draw_segments(im_out, segments)
+    cv.imwrite(filename, im_out)
+
+
+def filter_segment_at_rect(im, segments, rect):
+    cv.rectangle(skeleton, rect.p1.to_tuple(), rect.p2.to_tuple(), 100, -1)
+
+    def filter_segment(s):
+        r2 = Rectangle.from_segment(s)
+        return rect.collide(r2)
+    return filter(filter_segment, segments)
+
 
 if __name__ == '__main__':
-
-    s1 = Segment(Point(100, 100), Point(300, 300))
-    s2 = Segment(Point(200, 100), Point(200, 400))
-
-    print s1.intersection(s2)
 
     skeleton = process_skeleton()
 
@@ -273,28 +314,22 @@ if __name__ == '__main__':
     for x1, y1, x2, y2 in lines[0]:
         segments.append(Segment(Point(x1, y1), Point(x2, y2)))
 
-    # segments = segments[105:107] + segments[10:15]
-    # segments = segments[10:30] + segments[100:110]
+    # Filter Segments for DEBUG
+    # segments = filter_segment_at_rect(skeleton, segments, Rectangle(Point(1148, 340), Point(1400, 413)))
 
-    # segments = [
-    #     Segment(Point(100, 100), Point(100, 200)),
-    #     Segment(Point(0, 150),   Point(400, 150)),
-    #     Segment(Point(300, 0),   Point(300, 149)),
-    #     Segment(Point(401, 150), Point(401, 200)),
-    #     Segment(Point(350, 152), Point(450, 152)),
-    #     Segment(Point(350, 150), Point(400, 200)),
-    #     Segment(Point(400, 200), Point(500, 300)),
-    # ]
-    # skeleton = np.zeros((500, 500), dtype=np.uint8)
+    template = cv.cvtColor(skeleton, cv.COLOR_GRAY2RGBA)
+    base = cv.imread('Resources/1.bmp')
+    print base.shape
+    base = cv.cvtColor(base, cv.COLOR_RGB2RGBA)
+    template = base+template
 
 
-    im_vec = cv.cvtColor(skeleton, cv.COLOR_GRAY2RGB)
-    draw_segments(im_vec,  segments)
-    cv.imwrite('Resources/vec.png', im_vec)
+    write_segments(template, 'Resources/vec.png', segments)
 
     segments = clean_segments(segments)
+
+    write_segments(template, 'Resources/clean.png', segments)
+
     segments = connect_segments(segments)
 
-    im_seg = cv.cvtColor(skeleton, cv.COLOR_GRAY2RGB)
-    draw_segments(im_seg, segments)
-    cv.imwrite('Resources/seg.png', im_seg)
+    write_segments(template, 'Resources/seg.png', segments)
